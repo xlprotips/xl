d54bb1b5a7514ec788c8b258f89e9669f73f1183
Kevin Ryan <614956+ktr@users.noreply.github.com>
2021-07-16 20:13:18 -0400

    Changing function names converting column letters to numbers (and vice versa)
    These names are consistent with python sxl library and seem easier to remember.

--- src/sxl.rs
+++ src/sxl.rs
@@ -44,72 +44,72 @@ mod tests {
         use super::super::*;
         #[test]
         fn num_to_letter_w() {
-            assert_eq!(col_num_to_letter(23), Some(String::from("W")));
+            assert_eq!(num2col(23), Some(String::from("W")));
         }
 
         #[test]
         fn num_to_letter_aa() {
-            assert_eq!(col_num_to_letter(27), Some(String::from("AA")));
+            assert_eq!(num2col(27), Some(String::from("AA")));
         }
 
         #[test]
         fn num_to_letter_ab() {
-            assert_eq!(col_num_to_letter(28), Some(String::from("AB")));
+            assert_eq!(num2col(28), Some(String::from("AB")));
         }
 
         #[test]
         fn num_to_letter_xfd() {
-            assert_eq!(col_num_to_letter(16384), Some(String::from("XFD")));
+            assert_eq!(num2col(16384), Some(String::from("XFD")));
         }
 
         #[test]
         fn num_to_letter_xfe() {
-            assert_eq!(col_num_to_letter(16385), None);
+            assert_eq!(num2col(16385), None);
         }
 
         #[test]
         fn num_to_letter_0() {
-            assert_eq!(col_num_to_letter(0), None);
+            assert_eq!(num2col(0), None);
         }
 
         #[test]
         fn letter_to_num_w() {
-            assert_eq!(col_letter_to_num("W"), Some(23));
+            assert_eq!(col2num("W"), Some(23));
         }
 
         #[test]
         fn letter_to_num_aa() {
-            assert_eq!(col_letter_to_num("AA"), Some(27));
+            assert_eq!(col2num("AA"), Some(27));
         }
 
         #[test]
         fn letter_to_num_ab() {
-            assert_eq!(col_letter_to_num("AB"), Some(28));
+            assert_eq!(col2num("AB"), Some(28));
         }
 
         #[test]
         fn letter_to_num_xfd() {
-            assert_eq!(col_letter_to_num("XFD"), Some(16384));
+            assert_eq!(col2num("XFD"), Some(16384));
         }
 
         #[test]
         fn letter_to_num_xfe() {
-            assert_eq!(col_letter_to_num("XFE"), None);
+            assert_eq!(col2num("XFE"), None);
         }
 
         #[test]
         fn letter_to_num_ab_lower() {
-            assert_eq!(col_letter_to_num("ab"), Some(28));
+            assert_eq!(col2num("ab"), Some(28));
         }
 
         #[test]
         fn letter_to_num_number() {
-            assert_eq!(col_letter_to_num("12"), None);
+            assert_eq!(col2num("12"), None);
         }
 
         #[test]
         fn letter_to_num_semicolon() {
-            assert_eq!(col_letter_to_num(";"), None);
+            assert_eq!(col2num(";"), None);
         }
     }
 
@@ -168,7 +168,7 @@ mod tests {
 
 
 /// Return column letter for column number `n`
-pub fn col_num_to_letter(n: u16) -> Option<String> {
+pub fn num2col(n: u16) -> Option<String> {
     if n > XL_MAX_COL || n < XL_MIN_COL { return None }
     let mut s = String::new();
     let mut n = n;
@@ -182,7 +182,7 @@ pub fn col_num_to_letter(n: u16) -> Option<String> {
 
 
 /// Return column number for column letter `letter`
-pub fn col_letter_to_num(letter: &str) -> Option<u16> {
+pub fn col2num(letter: &str) -> Option<u16> {
     let letter = letter.to_uppercase();
     let re = Regex::new(r"[A-Z]+").unwrap();
     if !re.is_match(&letter) { return None }
